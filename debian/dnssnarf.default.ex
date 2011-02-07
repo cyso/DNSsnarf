b3fc15b84cc70471aa43d69d5dfd9c416bf4ef0a
Nick Douma <nick@development>
2011-02-07 16:52:59 +0100

    Add Debian package control files

---
+++ debian/dnssnarf.default.ex
@@ -0,0 +1,10 @@
+# Defaults for dnssnarf initscript
+# sourced by /etc/init.d/dnssnarf
+# installed at /etc/default/dnssnarf by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(dnssnarf):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="dnssnarf" command="/usr/bin/dnssnarf"
