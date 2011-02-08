db2c29097750f64bffc443e42e46f9cfc1688b69
Nick Douma <nick@development>
2011-02-08 15:27:53 +0100

    Exclude .git directory from source package and remove example files

--- debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the dnssnarf package
-#
-0 4	* * *	root	[ -x /usr/bin/dnssnarf_maintenance ] && /usr/bin/dnssnarf_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(dnssnarf):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="dnssnarf" command="/usr/bin/dnssnarf"
