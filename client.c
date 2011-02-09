1bf09cf96e8ee69aa34dd6cbd8cb94328129c0f5
peter <peter@cyso.nl>
2011-02-09 16:38:16 +0100

    Add client commandline options as already specified in manpage

--- client.c
+++ client.c
@@ -7,6 +7,8 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "types.h"
 #include "dns.h"
@@ -15,6 +17,10 @@
 
 uint64_t *qcounter;
 uint64_t *acounter;
+
+#define MODE_QUERY  1
+#define MODE_ANSWER 2
+#define MODE_TOTAL  3
 
 void dump_counters() {
 	int i;
@@ -38,18 +44,41 @@ void dump_counters() {
 void usage(char *prog) {
 	fprintf(
 		stderr,
-		"Usage   : %s <recordtype> <direction>\n"
-		"Examples: %s CNAME query\n"
-		"          %s CNAME answer\n\n", prog, prog, prog
+		"Usage   : %s <options>\n\n"
+	  "Options : -r RECORDTYPE\n"
+		"          -q Count of incoming packets\n"
+		"          -a Count of outgoing packets\n"
+		"          -t Combined count\n\n", prog
 	);
 }
 
 int main(int argc, char *argv[]) {
-	int shmid, i, rec_idx=-1, rec_dir=-1;
+	int c, mode = 0, shmid, i, rec_idx=-1; 
 	char *shm;
+	char *record_type = NULL;
+	uint64_t total = 0;
 
-	// check no. of params
-	if (argc != 3) {
+	while((c = getopt(argc, argv,  "qatr:")) != -1) {
+		switch(c) {
+			case 'r':
+				record_type = optarg;
+			break;
+
+			case 'q':
+				mode |= MODE_QUERY;			
+			break;
+
+			case 'a':
+				mode |= MODE_ANSWER;
+			break;
+
+			case 't':
+				mode |= MODE_TOTAL;
+			break;
+		}
+	}
+
+	if (record_type == NULL || mode == 0) {
 		usage(argv[0]);
 		exit(-1);
 	}
@@ -59,7 +88,7 @@ int main(int argc, char *argv[]) {
 		if (dns_record_type_name[i] == NULL)
 			continue;
 
-		if (strcmp(dns_record_type_name[i], argv[1]) == 0) {
+		if (strcmp(dns_record_type_name[i], record_type) == 0) {
 			rec_idx = i;
 			break;
 		}
@@ -68,16 +97,6 @@ int main(int argc, char *argv[]) {
 	if (rec_idx == -1) {
 		fprintf(stderr, "Invalid recordtype: '%s'\n", argv[1]);
 		exit(-1);
-	}
-
-	// check direction parameter validity
-	if (strcmp(argv[2], "query") == 0)
-		rec_dir = 0;
-	else if (strcmp(argv[2], "answer") == 0)
-		rec_dir = 1;
-
-	if (rec_dir == -1) {
-		fprintf(stderr, "Invalid direction: '%s'\n", argv[2]);
 	}
 
 	// grab shm handle and all that
@@ -95,12 +114,13 @@ int main(int argc, char *argv[]) {
 	qcounter = (uint64_t*)shm;
 	acounter = (qcounter + 256);
 
-	// dump requested counter to stdout as asciihex
-	if (rec_dir == 0)
-		printf("%llx\n", qcounter[rec_idx]);
-	else
-		printf("%llx\n", acounter[rec_idx]);
+	if (mode & MODE_QUERY)
+		total += qcounter[rec_idx];
 
+	if (mode & MODE_ANSWER)
+		total += acounter[rec_idx];
+
+	printf("%llx\n", total);
 	
 	return 0;
 }
